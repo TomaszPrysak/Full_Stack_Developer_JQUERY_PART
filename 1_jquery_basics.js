886df5628c3ed8126c5258bf67621cb09f1385ea
Tomek <prysak.tomasz@gmail.com>
2018-07-25 20:28:36 +0200

    Początek nauki jQuery

---
+++ 1_jquery_basics.js
@@ -0,0 +1,28 @@
+// jQuery
+//
+// jQuery to nic innego jak JavaScript !!! Tak tak, juz to umiemy :D
+// jdnak nie w takiej czystej postaci jaką umiemy.
+// Głównym przeznaczeniem jQuery jest uczynienie JavaScript łatwym w uzyciu.
+// Dzieje się tak, ponieważ jQuery jest biblioteką funkcji, metod i poleceń JavaScript.
+// Za pomocą tak przygotowanej biblioteki JavaScript możemy wykonywać wymagających
+// wielu lini kodu JavaScript zadań w prostszy sposób, ponieważ zostały one
+// spakowane w metody wywoływane pojedynczym poleceniem jQuery.
+// A więc biblioteki jQuery upraszczają wiele skomplikowanych rzeczy JavaScript
+// takich jak manipulowanie HTML DOM, wywoływania AJAX itd.
+
+// Biblioteki jQuery zawierają funkcje do:
+// - manipulowania elementami HTML, czyli mamy dostęp do HTML DOM,
+// - manipulowania stylami CSS,
+// - dodawania reakcji na zdarzenia HTML, tak samo jako w HTML DOM tylko prościej,
+// - tworzenie efektów i animacji,
+// - AJAX (o tym później),
+// - oraz wiele innych narzędzi.
+
+// A więc jeszcze raz, jQuery to biblioteki zawierające kod JavaScript,
+// ktory wywołujemy za pomocą specjalnych poleceń. Dzięki temu mamy oszczędność
+// w ilości napisanego kody.
+
+// 1. Podpinanie pliku z bibliotekami jQuery
+
+// Podobnie jak plik JavaScript podpinamy w pliku HTML w znacznikach SCRIPT,
+// tak samo
